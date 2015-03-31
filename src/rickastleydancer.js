905907a43a51ed1eaa943baa94efa8dfa4d07e49
Matthew Rourke <mrourke@gmail.com>
2015-03-31 00:00:56 -0400

    New Dancer RickAstley

---
+++ src/rickastleydancer.js
@@ -0,0 +1,25 @@
+var makeRickAstleyDancer = function(top, left, timeBetweenSteps){
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  this.$node.attr("id", "rickastley");
+  
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  //this.oldStep = this.step;
+
+};
+
+makeRickAstleyDancer.prototype = Object.create(makeDancer.prototype);
+
+makeRickAstleyDancer.prototype.constructor = makeRickAstleyDancer;
+
+makeRickAstleyDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.step.call(this);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
+
