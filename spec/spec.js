3d9cb5ebb307b5ccd86b893e90e638d882b0aeb6
Matthew Rourke <mrourke@gmail.com>
2015-03-30 23:54:32 -0400

    Added spec tests

--- spec/spec.js
+++ spec/spec.js
@@ -71,3 +71,40 @@ describe("lilJohnDancer", function() {
     });
   });
 });
+
+describe("rickAstleyDancer", function() {
+
+  var rickAstleyDancer;
+  var timeBetweenSteps = 100;
+  var clock;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    rickAstleyDancer = new makeRickAstleyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it("should have a jQuery $node object", function(){
+    expect(rickAstleyDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it("should have a step function that makes its node blink", function() {
+    sinon.spy(rickAstleyDancer.$node, 'toggle');
+    rickAstleyDancer.step();
+    expect(rickAstleyDancer.$node.toggle.called).to.be.true;
+  });
+
+  describe("dance", function(){
+    it("should call step at least once per second", function(){
+      //debugger;
+      sinon.spy(rickAstleyDancer, "step");
+      expect(rickAstleyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(rickAstleyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(rickAstleyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
