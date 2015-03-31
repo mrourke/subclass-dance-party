c30bdda650b1c1b833aed41e28d561851fe27d36
Matthew Rourke <mrourke@gmail.com>
2015-03-30 23:56:18 -0400

    Corrected blinky dancer id attribute addition

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,7 +1,8 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+  this.$node.attr("id", "blinkyDancer");
+  
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
