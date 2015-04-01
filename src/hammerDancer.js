ea16dab848ae1c959d690f13486ce6aae1569dac
Matthew Rourke <mrourke@gmail.com>
2015-03-31 20:35:28 -0400

    Cleaned up hammer animations

--- src/hammerDancer.js
+++ src/hammerDancer.js
@@ -2,10 +2,10 @@ var makeHammerDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
   this.$node.attr("id", "hammer");
-  
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-
+  this.move = 0;
+  this.timeBetweenSteps = 2000;
   //this.oldStep = this.step;
 
 };
@@ -20,6 +20,19 @@ makeHammerDancer.prototype.step = function(){
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  
+  var leftStr = this.$node.css('left');
+  var left = parseFloat(leftStr.slice(0,leftStr.length-3));
+  if (this.move) {
+    left = "" + (left+100) + "px";
+    this.move--;
+  } else {
+    left = "" + (left-100) + "px";
+    this.move++;
+  }
+  this.$node.animate({'left': left}, 750);
 };
+
+
+
+
 
